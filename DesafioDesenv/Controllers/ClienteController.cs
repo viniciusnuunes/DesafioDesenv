bf21e65c327afa60e9a6971334c84937650093c4
vinicius nunes <viniiciuspereiira@hotmail.com>
2017-07-13 19:10:13 -0300

    Adicionado a String de conexão e View Index renderizadando

---
+++ DesafioDesenv/Controllers/ClienteController.cs
@@ -0,0 +1,20 @@
+﻿using DesafioDesenv.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DesafioDesenv.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private DataContext contexto = new DataContext();
+
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View(contexto.Clientes.ToList());
+        }
+    }
+}
