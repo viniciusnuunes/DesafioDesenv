5bffdef3f9f631164047fa99f0dc97876a51e30a
vinicius nunes <viniiciuspereiira@hotmail.com>
2017-07-13 18:50:46 -0300

    Criado a Model e classe de Contexto. Adicionado Entity Framework

---
+++ DesafioDesenv/Models/Cliente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DesafioDesenv.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int clienteID { get; set; }
+
+        [Required]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Required]
+        [Display(Name = "Sobrenome")]
+        public string Sobrenome { get; set; }
+
+        [Required]
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.DateTime)]
+        public DateTime dataDeNascimento { get; set; }
+
+        [Required]
+        [Display(Name = "Telefone")]
+        [DataType(DataType.PhoneNumber)]
+        public int Telefone { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
---
+++ DesafioDesenv/Repository/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using DesafioDesenv.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace DesafioDesenv.Repository
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("ClienteDatabase")
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Add<PluralizingTableNameConvention>();
+        }
+    }
+}
