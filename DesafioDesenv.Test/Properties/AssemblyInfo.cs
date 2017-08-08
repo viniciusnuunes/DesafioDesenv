18f504270ccf8a562c0351149148637b39182b20
vinicius nunes <viniiciuspereiira@hotmail.com>
2017-08-08 19:37:43 -0300

    Commit inicial para testes (não funcionam)

---
+++ DesafioDesenv.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DesafioDesenv.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DesafioDesenv.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a6b0b830-f371-4627-bfdb-1114ed64e1ef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DesafioDesenv.Test/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesafioDesenv.Controllers;
+using System.Web.Mvc;
+using DesafioDesenv.Models;
+using Moq;
+using DesafioDesenv.Repository;
+
+namespace DesafioDesenv.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CreateTestMethod()
+        {
+            var MockRepository = new Mock<DataContext>();
+            var controller = new ClienteController(MockRepository.Object);
+
+            var clienteCreate = new Cliente()
+            {
+                clienteID = 1,
+                Nome = "Vinicius",
+                Sobrenome = "Nunes",
+                dataDeNascimento = "1993/07/22",
+                Email = "teste@teste.com.br",
+                Telefone = 980346645
+            };
+
+            var result = controller.Create(clienteCreate);
+
+            Assert.AreEqual("Create", result.ViewName);
+        }
+    }
+}
